91060b020e7b4b22d1b0bbffdb5274f402b6c4f0
kgwald <kristin.grunewald@gmail.com>
2019-01-02 18:00:28 -0600

    Created Cleaning files for Evening and Morning solving data. Integrated that data into the SolvingAnalysis script. Output additional tables for Solving by Insight/Analysis, Closeness Ratings, Stuckedness Ratings, and Solution Goodness Ratings.

--- Overnight2018CleanMorningData.R
+++ Overnight2018CleanMorningData.R
@@ -1,8 +1,8 @@
-#Function for cleaning Overnight 2018 Evening Solving Data
+#Function for cleaning Overnight 2018 Morning Solving Data
 
 #Before this script is run:
-#1) Overnight2018ReadPresentationData.R is run and outputs the file RawEveningData_compiled.csv
-#2) No human editing is necessary for the overall EveningData
+#1) Overnight2018ReadPresentationData.R is run and outputs the file RawMorningData_compiled.csv
+#2) No human editing is necessary for the overall MorningData
 #3) A human does open and edit RawAttemptData_compiled.csv and enters the experimenter comments for each puzzle. This
 #     data is then saved as AttemptData_edited.csv and is used for both the Evening and Morning Solving analyses.
 #4) All item-specific adjustments in this script are a result of experimenter comments and can be checked by:
@@ -11,7 +11,7 @@
 #     c) listening to the Solving sound recording files
 #     d) checking the participants' scrap paper
 
-Overnight2018CleanEveningData <- function(){
+Overnight2018CleanMorningData <- function(){
   
   #suppresses the column information from the readr read_csv function
   options(readr.num_columns = 0) 
@@ -28,13 +28,12 @@ Overnight2018CleanEveningData <- function(){
   
   
   #Read in the raw EveningData file
-  rawdata <- read_csv("RawEveningData_compiled.csv")
+  rawdata <- read_csv("RawMorningData_compiled.csv")
   #Read in edited AttemptData file (for comparison)
   attemptdata <- read_csv("AttemptData_edited.csv")
   
   #For consisency with other scipts, set editeddata = rawdata and then edit it
   editeddata <- rawdata
-  
   
   ##### Add/Edit Columns #####
   
@@ -42,24 +41,18 @@ Overnight2018CleanEveningData <- function(){
   editeddata <- editeddata %>%
     mutate(Solved = ifelse(TrialStatus == "solved",1,0))
   
-  #Change InsightorAnalysisResp 1's to "insight" and 2's to "analysis"
+  #Change 1's to "insight" and 2's to "analysis"
   editeddata$InsightorAnalysisResp <- gsub("1","insight", editeddata$InsightorAnalysisResp)
   editeddata$InsightorAnalysisResp <- gsub("2","analysis", editeddata$InsightorAnalysisResp)
   
-  #Change FixationRatingResp 1's to "stillworking" and 2's to "stuck"
-  editeddata$FixationRatingResp <- as.character(editeddata$FixationRatingResp)
-  editeddata$FixationRatingResp <- gsub("1","stillworking", editeddata$FixationRatingResp)
-  editeddata$FixationRatingResp <- gsub("2","stuck", editeddata$FixationRatingResp) 
-  
   ###### Solving Comments Adjustments #####
   
-  #8002 E2 M13 should be solved by analysis not insight (changed her mind immediately after 'insight' was recorded)
-  editeddata$InsightorAnalysisResp[editeddata$SID =="8002" & editeddata$ProblemID == "M13d" & editeddata$Session == "e2"] <- "analysis"
-  
-  #8005 E1 M08d participant solved the puzzle on the closeness rating screen - not solved in the Evening so leave as
-  #'unsolved' but exclude from morning data
+  #8005 E1/M1 M08d participant solved the puzzle on the closeness rating screen - too late to mark as solved
+  #for the evening, exclude from morning data
+  editeddata$TrialStatus[editeddata$SID =="8005" & editeddata$Session == "m1" & editeddata$ProblemID == "M08d"] <- "exclude-solvedlastnight"
   
   setwd(startdir)
   return(editeddata)
   
-}
+} 
+  
